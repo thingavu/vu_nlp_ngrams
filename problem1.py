4e53515f93262033585d1d9cf3b17f53475c2c18
BCJACK0125 <110091749+BCJACK0125@users.noreply.github.com>
2024-04-23 17:05:55 +0200

    Update problem1 answer

--- problem1.py
+++ problem1.py
@@ -11,8 +11,19 @@ DO NOT SHARE/DISTRIBUTE SOLUTIONS WITHOUT THE INSTRUCTOR'S PERMISSION
 word_index_dict = {}
 
 # TODO: read brown_vocab_100.txt into word_index_dict
+with open("brown_vocab_100.txt", "r") as file:
+    lines = file.readlines()
+    for index, line in enumerate(lines):
+        line = line.rstrip('\n')
+        word_index_dict[line] = index
 
 # TODO: write word_index_dict to word_to_index_100.txt
+with open("word_to_index_100.txt", "w") as wf:
+    for word, index in word_index_dict.items():
+        wf.write(str(word)+" "+str(index))
+        wf.write('\n')
+
+
 
 print(word_index_dict['all'])
 print(word_index_dict['resolution'])
